4feed585db376442b59a63af44e5d92fceddaa54
valencra <rafv2172@gmail.com>
2015-12-20 17:17:15 -0500

    completed cost and gradient functions for regularized logistic regression

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -16,10 +16,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
-
-
-
-
+hypothesis = sigmoid(X*theta);
+J = (1/m)*sum(-y.*log(hypothesis)-(1-y).*log(1-hypothesis))+lambda/(2*m)*sum(theta(2:end).^2);
+error = hypothesis-y;
+grad(1) = (1/m)*X(:,1)'*error;
+grad(2:end) = (1/m)*X(:,2:end)'*error+(lambda/m)*theta(2:end);
 
 
 % =============================================================
