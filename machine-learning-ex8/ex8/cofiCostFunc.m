f123b8bd77f2646b3255672597c45ea86404c205
valencra <rafv2172@gmail.com>
2016-01-28 21:48:51 -0500

    Completed gradient calculation code

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,19 +41,8 @@ Theta_grad = zeros(size(Theta));
 %
 
 J = (1/2)*sum(sum(((X*Theta'-Y).*R).^2)); % cost function
-
-
-
-
-
-
-
-
-
-
-
-
-
+X_grad = ((X*Theta'-Y).*R)*Theta; % X gradients
+Theta_grad = ((X*Theta'-Y).*R)'*X; % Theta gradients
 
 % =============================================================
 
